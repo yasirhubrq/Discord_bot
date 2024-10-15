e2562830989eeec1064105acf12b16eb31d771dd
yasir <86596211+yasirhubrq@users.noreply.github.com>
2024-10-15 23:22:59 +0500

    Update main.py
    added discription to the code

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
+#importing all the modules and libraries
 import discord
 from discord.ext import commands
 import os
 from dotenv import load_dotenv
-
+#to load the token from .env file
 load_dotenv
 
 TOKEN = os.getenv('DISCORD_TOKEN')
-
+#giving permission to bot to read and write messages
 intents = discord.Intents.default()
 intents.message_content = True
 bot = commands.Bot(command_prefix='/', intents=intents)
-
+#creating commands
 @bot.command(name='test')
 async def test(ctx):
     await ctx.send('pong')
@@ -22,5 +23,5 @@ async def loots(ctx):
 @bot.command(name='hello')
 async def hello(ctx):
     await ctx.raect('heart')
-
+#bot token and runn
 bot.run('MTI5NTc0NzY2NzcyNDMzNzE1Mw.Gw8h0b.LLQsocsSOt2NvWsFdx0GhP2XgJspiW-r8AKxIE')
