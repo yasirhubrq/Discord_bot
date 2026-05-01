4d61dc6a224713fcc2e2feca740dc02a53b32aa1
yasir <86596211+yasirhubrq@users.noreply.github.com>
2026-05-01 21:46:20 +0500

    Refactor Discord bot to use new client structure
    Refactor bot commands and update client initialization.

--- main.py
+++ main.py
@@ -1,27 +1,34 @@
-#importing all the modules and libraries
-import discord
+import discord as dc
 from discord.ext import commands
-import os
-from dotenv import load_dotenv
-#to load the token from .env file
-load_dotenv
-
-TOKEN = os.getenv('DISCORD_TOKEN')
-#giving permission to bot to read and write messages
-intents = discord.Intents.default()
+import random
+import scrapss
+TOKEN = '' #replace with your bot token
+__init__ = __name__
+intents = dc.Intents.default()
 intents.message_content = True
-bot = commands.Bot(command_prefix='/', intents=intents)
-#creating commands
-@bot.command(name='test')
-async def test(ctx):
-    await ctx.send('pong')
 
-@bot.command(name='loots')
-async def loots(ctx):
-    await ctx.send('bruh')
+client = commands.Bot(command_prefix='!', intents=intents)
+#this event is used to print a message when the bot is ready and logged in
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+#this command is used to get a random meme from the imgflip api and send it as an embed in the discord channel
+@client.command()
+async def meme(ctx):
+    embed = dc.Embed(title="Here's a meme for you!",color=0x00ff00)
+    embed.set_image(url=scrapss.get_meme())
+    await ctx.send(embed=embed)
+#this command is used to get the current players in the game and their details
+@client.command()
+async def players(ctx):
+    embed2 = dc.Embed(title="Current Players in Game",description=scrapss.player_det(),color=0x0000ff)
+    embed2.add_field(name="", value="", inline=False)
+    await ctx.send(embed=embed2)
+#this command is used to get the location of an IP address
+@client.command()
+async def locate(ctx, ip_address):
+    location_info = scrapss.ip_locate(ip_address)
+    await ctx.send(f"Location info for IP {ip_address}: {location_info}")
 
-@bot.command(name='hello')
-async def hello(ctx):
-    await ctx.raect('heart')
-#bot token and runn
-bot.run('')
+
+client.run(TOKEN)
